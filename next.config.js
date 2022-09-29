ba5c6339ec295c03954dfdc30bea9129dbd5bd45
Stefan Nasturas <klazer220033@gmail.com>
2022-09-29 12:17:54 +0300

    Catto Commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  scope: '/',
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ['cdn2.thecatapi.com', 'http.cat'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
